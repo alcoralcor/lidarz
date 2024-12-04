cf86ba4b70caeeb0973d6ecc6cb8eedf0d00102b
Alcor WALTER <alcor@hiphouse.org>
2024-12-04 23:20:54 +0100

    new version with filters, offset, ini config and multiple lidars. lots of fixes too (360° messages, etc)

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # What is lidar_wrtc ?  
 **Warning:** I'm not a developer!  
-A small Python proof of concept that reads binary data from an OKDO LDO6 LIDAR via the serial port.
+A small Python proof of concept that reads binary data from at least one OKDO LDO6 LIDAR via the serial port(s).
 The coordinates are extracted, decoded, converted to Cartesian coordinates and transmitted in batches of 360° measurements to a web page via a WebRTC Data Channel.
-The web page (an HTML file with embedded Javascript), is served by the same Python script in HTTP on port 8080. A graphical display shows the real-time measurements taken by the LIDAR.  
-The "LIDAR" code is largely based on James Gibbard's (https://gibbard.me/lidar/).  
+A polygonal filter and an offset position can be applied to each LIDAR.
+The web page (an HTML file with embedded Javascript), is served by the same Python script in HTTP on port 8080. A graphical display shows the real-time measurements taken by the LIDAR(s).  
+The "LIDAR" decoding is largely based on James Gibbard's (https://gibbard.me/lidar/).  
+Many settings can be done using a .ini file
 
 ## Requirements :
-- OKDO LIDAR LD06 (small, inexpensive LIDAR)
-- USB / Serial dongle
+- at least one OKDO LIDAR LD06 (small, inexpensive LIDAR)
+- one USB / Serial dongle per LIDAR
 - Python 3.12 (likely to run with earlier versions)
   
 ## Planned evolutions :
-- Filter the area to be monitored
+- run a live server with restart of the data channel to be renegotiated.
+- sort python debug and change default name
+- enable plotly graph setting
+- color points according to lidars?
+- allowing to choose an .ini parameter file
 - Manage script termination
 - Write Python/HTML/Javascript code cleanly (I need help)
 - Optimize code
-  
+
+
+## Planned tests :
+- tests on Windows and MacOS
+- check lidar positioning and filters
+- test with 3 lidars
+- test with 1 lidar
+- test with non-orthogonal shape filters
+
 ## Possible evolutions :
 - Object tracking
 - Multi-zone triggering
@@ -33,10 +47,7 @@ python -m pip install -r requirements.txt
 ```
 
 ## Running (until Ctrl+C)
-Before running server, you may edit lidar_wrtc.py to change some values :
-- SERVER_IP = "0.0.0.0"
-- SERVER_PORT = 8080
-- SERIAL_PORT = "/dev/ttyUSB0"
+Before running server, you may edit lidar_wrtc.ini to adapt according to your needs.
   
 ```bash
 python lidar_wrtc.py
--- lidar_wrtc.py
+++ lidar_wrtc.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+import os
+import argparse
+import configparser
+import logging
 import asyncio
 import json
 import serial_asyncio_fast
@@ -5,134 +10,159 @@ import numpy as np
 import struct
 from enum import Enum
 from json import JSONEncoder
-import os
+from shapely.geometry import Point, Polygon
+from shapely.prepared import prep
 from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription
 
-PRINT_DEBUG = False
-
-SERVER_IP = "0.0.0.0"
-SERVER_PORT = 8080
-
-SERIAL_PORT = "/dev/ttyUSB0"
-SERIAL_BAUDRATE=230400
-MEASUREMENTS_PER_PLOT = 480
-
 MESSAGE_LENGTH = 47
+
 MEASUREMENT_LENGTH = 12 
 MESSAGE_FORMAT = "<xBHH" + "HB" * MEASUREMENT_LENGTH + "HHB"
 
-State = Enum("State", ["SYNC0", "SYNC1", "SYNC2", "LOCKED", "UPDATE_PLOT", "WS_SEND"])
-state = State.SYNC0
-message = b'' 
-message_pos = 0
+logger = logging.getLogger("lidar_webrtc_filter.py")
 
-measurements = []
+State = Enum("State", ["SYNC0", "SYNC1", "SYNC2", "LOCKED", "UPDATE_PLOT", "WS_SEND"])
 
 ROOT = os.path.dirname(__file__)
 
 pc = None
 dc = None
+serials = []
 
-# Serial read
+# Lidar Serial read
 
-class SerialProtocol(asyncio.Protocol):
+class LidarSerialProtocol(asyncio.Protocol):
+    name = None
+    prepared_polygon = None
+    offset = np.array([0.0, 0.0])
+
+    def __init__(self):
+        super().__init__()
+        self.transport = None
+        self.state = State.SYNC0
+        self.lidar_message = b'' 
+        self.lidar_message_pos = 0
+        self.polar_coords = []
+        self.next_polar_coords = []
 
     def connection_made(self, transport):
+        logger.debug('port opened')
         self.transport = transport
-        if PRINT_DEBUG: print('port opened', transport)
 
     def connection_lost(self, exc):
-        if PRINT_DEBUG: print('port closed')
+        logger.debug('port closed')
         self.transport.loop.stop()
 
-    def data_received(self, data):
-        global state, message, message_pos, measurements, dc, pc
-        data_pos = 0
-        data_len = len(data)
-        if PRINT_DEBUG: print('data received', repr(data), data_len, data_pos, len(message), message_pos)
-        while data_pos < data_len:
-            if state == State.SYNC0:
-                message = b''
-                message_pos = 0
-                if data[data_pos:data_pos+1] == b'\x54':
-                    message = b'\x54'
-                    message_pos += 1
-                    if PRINT_DEBUG: print(state, message, data_len, data_pos, len(message), message_pos)
-                    state = State.SYNC1
+    def data_received(self, serial_data):
+        global dc, pc
+        serial_data_pos = 0
+        serial_data_len = len(serial_data)
+        logger.debug('data received', repr(serial_data), serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+        while serial_data_pos < serial_data_len:
+            if self.state == State.SYNC0:
+                self.lidar_message = b''
+                self.lidar_message_pos = 0
+                if serial_data[serial_data_pos:serial_data_pos+1] == b'\x54':
+                    self.lidar_message = b'\x54'
+                    self.lidar_message_pos += 1
+                    logger.debug(self.state, self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+                    self.state = State.SYNC1
                 else:
-                    if PRINT_DEBUG: print(state, '\033[93m\033[1m' + "WARNING: Syncing" + '\033[0m', message, data_len, data_pos, len(message), message_pos)
-                data_pos += 1
+                    logger.debug(self.state, '\033[93m\033[1m' + "WARNING: Syncing" + '\033[0m', self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+                serial_data_pos += 1
 
-            elif state == State.SYNC1:
-                if data[data_pos:data_pos+1] == b'\x2C':
-                    message += b'\x2C'
-                    message_pos += 1
-                    if PRINT_DEBUG: print(state, message, data_len, data_pos, len(message), message_pos)
-                    state = State.SYNC2
+            elif self.state == State.SYNC1:
+                if serial_data[serial_data_pos:serial_data_pos+1] == b'\x2C':
+                    self.lidar_message += b'\x2C'
+                    self.lidar_message_pos += 1
+                    logger.debug(self.state, self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+                    self.state = State.SYNC2
                 else:
-                    if PRINT_DEBUG: print(state, '\033[93m\033[1m' + "WARNING: Second byte not expected" + '\033[0m', message, data_len, data_pos, len(message), message_pos)
-                    state = State.SYNC0
-                data_pos += 1
+                    logger.debug(self.state, '\033[93m\033[1m' + "WARNING: Second byte not expected" + '\033[0m', self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+                    self.state = State.SYNC0
+                serial_data_pos += 1
 
-            elif state == State.SYNC2:
-                if (data_len - data_pos) < (MESSAGE_LENGTH - message_pos):
-                    message += data[data_pos:]
-                    message_pos += (data_len - data_pos)
-                    data_pos = data_len
-                    if PRINT_DEBUG: print(state, "Message à completer", message, data_len, data_pos, len(message), message_pos)
+            elif self.state == State.SYNC2:
+                if (serial_data_len - serial_data_pos) < (MESSAGE_LENGTH - self.lidar_message_pos):
+                    self.lidar_message += serial_data[serial_data_pos:]
+                    self.lidar_message_pos += (serial_data_len - serial_data_pos)
+                    serial_data_pos = serial_data_len
+                    logger.debug(self.state, "Message LIDAR à completer", self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
                 else:
-                    message += data[data_pos:(data_pos + MESSAGE_LENGTH - message_pos)]
-                    data_pos += (MESSAGE_LENGTH - message_pos)
-                    measurements += parse_lidar_data(message)
-                    if PRINT_DEBUG: print(state, "Message complet", message, data_len, data_pos, len(message), message_pos)
-                    if len(measurements) > MEASUREMENTS_PER_PLOT:
-                        state = State.WS_SEND
+                    self.lidar_message += serial_data[serial_data_pos:(serial_data_pos + MESSAGE_LENGTH - self.lidar_message_pos)]
+                    serial_data_pos += (MESSAGE_LENGTH - self.lidar_message_pos)
+                    lidar_message_parsed = self.parse_lidar_data(self.lidar_message)
+                    if self.polar_coords != []:
+                        if lidar_message_parsed[0][0] < self.polar_coords[-1][0]:
+                            new_polar_coords = True
+                        else:
+                            new_polar_coords = False
                     else:
-                        state = State.SYNC0
-            elif state == State.WS_SEND:
-                full_xy = get_xy_data(measurements)
-                numpyData = {"array": full_xy}
+                        new_polar_coords = False
+                    for lidar_message_index in range(1, len(lidar_message_parsed)):
+                        if lidar_message_parsed[lidar_message_index][0] < 360.0:
+                            if new_polar_coords:
+                                self.next_polar_coords.append(lidar_message_parsed[lidar_message_index])
+                            else:
+                                self.polar_coords.append(lidar_message_parsed[lidar_message_index])
+                        else:
+                            self.next_polar_coords.append(tuple((lidar_message_parsed[lidar_message_index][0] - 360, lidar_message_parsed[lidar_message_index][1])))
+                    if self.next_polar_coords == []:
+                        self.state = State.SYNC0
+                    else:
+                        self.state = State.WS_SEND
+
+                    logger.debug(self.state, "Message LIDAR complet", self.lidar_message, serial_data_len, serial_data_pos, len(self.lidar_message), self.lidar_message_pos)
+
+            elif self.state == State.WS_SEND:
+                cartesian_coords = self.get_xy_data(self.polar_coords)
+                numpyData = {self.name: cartesian_coords}
                 encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)
                 if dc != None:
                     if dc.readyState == "open":
                         dc.send(encodedNumpyData)
                 else:
-                    if PRINT_DEBUG: print("NO DATA CHANNEL")
-                measurements = []
-                state = State.SYNC0
+                    logger.debug("NO DATA CHANNEL")
+                self.polar_coords = list(tuple(self.next_polar_coords))
+                self.next_polar_coords = []
+                self.state = State.SYNC0
 
 
-def parse_lidar_data(data):
-    # Extract data
-    length, speed, start_angle, *pos_data, stop_angle, timestamp, crc = \
-        struct.unpack(MESSAGE_FORMAT, data)
-    # Scale values
-    start_angle = float(start_angle) / 100.0
-    stop_angle = float(stop_angle) / 100.0
-    # Unwrap angle if needed and calculate angle step size
-    if stop_angle < start_angle:
-        stop_angle += 360.0
-    step_size = (stop_angle - start_angle) / (MEASUREMENT_LENGTH - 1)
-    # Get the angle for each measurement in packet
-    angle = [start_angle + step_size * i for i in range(0,MEASUREMENT_LENGTH)]
-    distance = pos_data[0::2] # in millimeters
-    if PRINT_DEBUG:
-        print("PARSING", length, speed, start_angle, *pos_data, stop_angle, timestamp, crc)
-    return list(zip(angle, distance))
+    def parse_lidar_data(self, data):
+        # Extract data
+        length, speed, start_angle, *pos_data, stop_angle, timestamp, crc = \
+            struct.unpack(MESSAGE_FORMAT, data)
+        # Scale values
+        start_angle = float(start_angle) / 100.0
+        stop_angle = float(stop_angle) / 100.0
+        # Unwrap angle if needed and calculate angle step size
+        if stop_angle < start_angle:
+            stop_angle += 360.0
+        step_size = (stop_angle - start_angle) / (MEASUREMENT_LENGTH - 1)
+        # Get the angle for each measurement in packet
+        angle = [start_angle + step_size * i for i in range(0,MEASUREMENT_LENGTH)]
+        distance = pos_data[0::2] # in millimeters
+        logger.debug("PARSING", length, speed, start_angle, *pos_data, stop_angle, timestamp, crc)
+        return list(zip(angle, distance))
 
 
-def get_xy_data(measurements):
-    # Unpack the tuples
-    angle = np.array([measurement[0] for measurement in measurements])
-    distance = np.array([measurement[1] for measurement in measurements])
-    # Convert to cartesian coordinates in meters
-    x = np.sin(np.radians(angle)) * (distance / 1000.0)
-    y = np.cos(np.radians(angle)) * (distance / 1000.0)
-    if PRINT_DEBUG:
-        print(x, y)
-        print(np.dstack((x, y)))
-    return np.dstack((x, y))
+    def get_xy_data(self, measurements):
+        angle = np.array([measurement[0] for measurement in measurements])
+        distance = np.array([measurement[1] for measurement in measurements])
+
+        x = np.sin(np.radians(angle)) * (distance / 1000.0)
+        y = np.cos(np.radians(angle)) * (distance / 1000.0)
+        stackxy = np.dstack((x, y))[0]
+
+        result = self.filter_coordinates_in_polygon(stackxy)
+        logger.debug(result)
+        return result
+
+
+    def filter_coordinates_in_polygon(self, coordinates):
+        filtered_coordinates = np.array([point.tolist() for point in coordinates if self.prepared_polygon.contains(Point(point))]) + self.offset
+        return filtered_coordinates
 
 
 class NumpyArrayEncoder(JSONEncoder):
@@ -141,17 +171,16 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
-# WebRTC send
 
 async def offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    if PRINT_DEBUG: print("Created for", request.remote)
+    logger.debug("Created for", request.remote)
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
-        if PRINT_DEBUG: print("Connection state is", pc.connectionState)
+        logger.debug("Connection state is", pc.connectionState)
         if pc.connectionState == "failed":
             await pc.close()
             pc.discard(pc)
@@ -168,6 +197,7 @@ async def offer(request):
         ),
     )
 
+
 async def on_shutdown(app):
     pc.close()
 
@@ -178,14 +208,43 @@ async def index(request):
 
 
 async def main():
-    global pc, dc
+    global dc, pc, serials
+
+    parser = argparse.ArgumentParser(
+        prog="Lidar WebRTC",
+        description="OKDO LD06 LIDAR to WebRTC Data Channel",
+        epilog="Have fun !"
+    )
+    parser.add_argument("-v", "--verbose", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    config = configparser.ConfigParser()
+    config.read('lidar_wrtc.ini')
 
     loop = asyncio.get_event_loop()
 
-    transport, protocol = await serial_asyncio_fast.create_serial_connection(loop, SerialProtocol, SERIAL_PORT, baudrate=SERIAL_BAUDRATE)
+    for section in config.sections():
+        if section == "WEBRTCSERVER":
+            server_host = config["WEBRTCSERVER"].get("server-host", "0.0.0.0")
+            server_port = config["WEBRTCSERVER"].getint("server-port", 8080)
+        elif section.startswith("LIDAR"):
+            serial_port = config[section].get("serial-port", "/dev/ttyUSB0")
+            serial_baudrate = config[section].getint("serial-baudrate", 230400)
+            filter = config[section].get("filter", "[(-12.0, -12.0), (-12.0, 12.0), (12.0, 12.0), (12.0, -12.0)]")
+            offset = config[section].get("offset", "[0.0, 0.0]")
+
+            lidar_filter = eval(filter)
+            polygon = Polygon(lidar_filter)    
+            prepared_polygon = prep(polygon)
+            lidar_offset = np.array(eval(offset))
+            serials.append(await serial_asyncio_fast.create_serial_connection(loop, type(str(section), (LidarSerialProtocol,),{"name": section, "prepared_polygon": prepared_polygon, "offset": lidar_offset}), url=serial_port, baudrate=serial_baudrate))
 
     pc = RTCPeerConnection()
-
     dc = pc.createDataChannel("lidar", negotiated=True, ordered=True, id=2) #
 
     @dc.on("open")
@@ -199,13 +258,15 @@ async def main():
 
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, host=SERVER_IP, port=SERVER_PORT)
+    site = web.TCPSite(runner, host=server_host, port=server_port)
     await site.start()
 
-    print("Server started. Point browser to", site.name)
+    logger.info("Server started. Point browser to " + site.name)
     await asyncio.Event().wait()
     loop.close()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
+
